b5a850aad205fba7997c4dd25ba7e0de3a85fc21
VajaDip <162326394+VajaDip@users.noreply.github.com>
2025-04-25 04:54:40 -0700

    Create Practical [6(2)B].php

---
+++ Practical [6(2)B].php
@@ -0,0 +1,13 @@
+ 
+<?php  
+func on reverseString($str) {  
+    $reversed = '';  
+    for ($i = strlen($str) - 1; $i >= 0; $i--) {  
+        $reversed .= $str[$i];  
+    }  
+    return $reversed;  
+}  
+  
+$input = "hello";  
+echo "Reversed: " . reverseString($input) . "<br>";  
+?>
